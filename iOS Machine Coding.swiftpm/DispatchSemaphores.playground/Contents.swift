51d1cd84821d676814a4dea8c9cc71c8a60195fe
patilvedant01 <vedantpatil324@gmail.com>
2026-01-31 00:35:02 +0530

    first commit for learnings

---
+++ iOS Machine Coding.swiftpm/DispatchSemaphores.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class Product {
+    var name: String
+    private var price: Int
+    private var semaphore: DispatchSemaphore = DispatchSemaphore(value: 1)
+    
+    init(name: String, price: Int) {
+        self.name = name
+        self.price = price
+    }
+    
+    func readPrice() -> Int {
+        semaphore.wait()
+        defer {
+            semaphore.signal()
+        }
+        return price
+    }
+    
+    func updatePrice(updatedPrice: Int) {
+        semaphore.wait()
+        defer {
+            semaphore.signal()
+        }
+        
+        self.price = updatedPrice
+    }
+}
